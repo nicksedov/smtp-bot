5e7361afc97986e05226c0990ab12d51cba63d56
root <nicksedov@yandex.ru>
2023-01-26 18:15:25 +0300

    Поддержка Markdown

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/nicksedov/sbconn-bot
 go 1.13
 
 require (
-	github.com/alash3al/go-smtpsrv v0.0.0-20220704173150-cdaad3f3f582 // indirect
+	github.com/alash3al/go-smtpsrv v0.0.0-20220704173150-cdaad3f3f582
 	github.com/go-resty/resty/v2 v2.3.0
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/miekg/dns v1.1.50 // indirect
--- smtphandler.go
+++ smtphandler.go
@@ -1,66 +1,67 @@
 package main
 
 import (
-	"errors"
-	"strconv"
-	"strings"
+    "errors"
+    "strconv"
+    "strings"
 
-	"github.com/alash3al/go-smtpsrv"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func smtpHandler(c *smtpsrv.Context) error {
-	msg, err := c.Parse()
-	if err != nil {
-		return errors.New("Cannot read your message: " + err.Error())
-	}
-	to := transformStdAddressToEmailAddress(msg.To)
-	chatId := lookupChatId(to)
-	if chatId == 0 {
-		cc := transformStdAddressToEmailAddress(msg.Cc)
-		chatId = lookupChatId(cc)
-	}
-	if chatId == 0 {
-		bcc := transformStdAddressToEmailAddress(msg.Bcc)
-		chatId = lookupChatId(bcc)
-	}
+    msg, err := c.Parse()
+    if err != nil {
+	return errors.New("Cannot read your message: " + err.Error())
+    }
+    to := transformStdAddressToEmailAddress(msg.To)
+    chatId := lookupChatId(to)
+    if chatId == 0 {
+	cc := transformStdAddressToEmailAddress(msg.Cc)
+	chatId = lookupChatId(cc)
+    }
+    if chatId == 0 {
+	bcc := transformStdAddressToEmailAddress(msg.Bcc)
+	chatId = lookupChatId(bcc)
+    }
 
-	from := strings.Join(extractEmails(msg.From), ", ")
-	if msg.HTMLBody != "" {
-		sendHtml(from, msg.Subject, msg.HTMLBody, chatId)
-	} else if msg.TextBody != "" {
-		sendText(from, msg.Subject, msg.TextBody, chatId)
-	}
-	return nil
+    from := strings.Join(extractEmails(msg.From), ", ")
+    if msg.HTMLBody != "" {
+	sendHtml(from, msg.Subject, msg.HTMLBody, chatId)
+    } else if msg.TextBody != "" {
+	text := strings.Split(msg.TextBody, "<!--- END OF DOCUMENT --->")[0]
+	sendText(from, msg.Subject, text, chatId)
+    }
+    return nil
 }
 
 func lookupChatId(addr []*EmailAddress) int64 {
-	var chatId int64
-	var err error
-	for _, a := range addr {
-		tokens := strings.Split(a.Address, "@")
-		if strings.HasPrefix(tokens[0], "chatid") {
-			chatId, err = strconv.ParseInt(strings.TrimPrefix(tokens[0], "chatid"), 10, 64)
-			if err == nil {
-				break
-			}
-		}
+    var chatId int64
+    var err error
+    for _, a := range addr {
+	tokens := strings.Split(a.Address, "@")
+	if strings.HasPrefix(tokens[0], "chatid") {
+	    chatId, err = strconv.ParseInt(strings.TrimPrefix(tokens[0], "chatid"), 10, 64)
+	    if err == nil {
+		break
+	    }
 	}
-	return chatId
+    }
+    return chatId
 }
 
 func sendHtml(from string, subj string, html string, chatId int64) {
-	file := tgbotapi.FileBytes{
-		Name:  "Сообщение.html",
-		Bytes: []byte(html),
-	}
-	doc := tgbotapi.NewDocument(chatId, file)
-	doc.Caption = "Сообщение от: " + from + "\nТема: " + subj
-	SendDocumentToChat(doc)
+    file := tgbotapi.FileBytes{
+	Name:  "Сообщение.html",
+	Bytes: []byte(html),
+    }
+    doc := tgbotapi.NewDocument(chatId, file)
+    doc.Caption = "**Сообщение от:** " + from + "\n**Тема:** " + subj + "\n***"
+    doc.ParseMode = "MarkdownV2"
+    SendDocumentToChat(doc)
 }
 
 func sendText(from string, subj string, text string, chatId int64) {
-	caption := "Сообщение от: " + from + "\nТема: " + subj + "\n"
-	textMsg := tgbotapi.NewMessage(chatId, caption+text)
-	SendMessageToChat(textMsg)
+    caption := "Сообщение от: " + from + "\nТема: " + subj + "\n"
+    textMsg := tgbotapi.NewMessage(chatId, caption+text)
+    SendMessageToChat(textMsg)
 }
