0d06fa3a9559ff7cf15e055dda00f6dcc54cd176
Nikolay Sedov <nicksedov@yandex.ru>
2023-01-28 00:29:18 +0300

    Added HTML caption

--- smtphandler.go
+++ smtphandler.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"html"
 	"strconv"
 	"strings"
 
@@ -50,26 +52,28 @@ func lookupChatId(addr []*EmailAddress) int64 {
 	return chatId
 }
 
-func sendHtml(from string, subj string, html string, chatId int64) {
-	htmlBody := getHtmlBody(html)
+func sendHtml(from string, subj string, htmlDoc string, chatId int64) {
+	htmlBody := getHtmlBody(htmlDoc)
 	if isSupportedMarkdown(htmlBody) {
-		textMsg := tgbotapi.NewMessage(chatId, htmlBody)
-		textMsg.ParseMode = "html"
+		caption := fmt.Sprintf("<b>Сообщение от:</b> %s\n<b>Тема:</b> %s\n", html.EscapeString(from), html.EscapeString(subj))
+		text := caption + htmlBody
+		textMsg := tgbotapi.NewMessage(chatId, text)
+		textMsg.ParseMode = "HTML"
 		SendMessageToChat(textMsg)
 	} else {
 		file := tgbotapi.FileBytes{
 			Name:  "Сообщение.html",
-			Bytes: []byte(html),
+			Bytes: []byte(htmlDoc),
 		}
 		doc := tgbotapi.NewDocument(chatId, file)
-		doc.Caption = "*Сообщение от:* " + from + "\n*Тема:* " + subj + "\n"
+		doc.Caption = fmt.Sprintf("*Сообщение от:* %s\n*Тема:* %s\n", from, subj)
 		doc.ParseMode = "markdown"
 		SendDocumentToChat(doc)
 	}
 }
 
 func sendText(from string, subj string, text string, chatId int64) {
-	caption := "*Сообщение от:* " + from + "\n*Тема:* " + subj + "\n"
+	caption := fmt.Sprintf("*Сообщение от:* %s\n*Тема:* %s\n", from, subj)
 	textMsg := tgbotapi.NewMessage(chatId, caption+text)
 	textMsg.ParseMode = "markdown"
 	SendMessageToChat(textMsg)
